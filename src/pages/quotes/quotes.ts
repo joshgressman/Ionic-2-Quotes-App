03e4b69a9d86830bc27b1ec94355b194b6ee5fe9
joshgressman <joshgressman@gmail.com>
2017-06-22 09:28:15 -0500

    added alerts within the quotes fav method

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @Component({
@@ -10,11 +10,37 @@ export class QuotesPage {
  quoteGroup: {category: string, quotes: Quote[], icon: string};
 
 //Get the data  from the library page-library
-constructor(private navParams: NavParams){
+constructor(private navParams: NavParams, private alertCtrl: AlertController){
     this.quoteGroup = this.navParams.data;
     //.data is what is being passed for the quoteGroup{}
     console.log(this.quoteGroup);
 }
 
+
+//setting up the alert on click to favorites
+onAddToFavorites(selectedQuote: Quote){
+  const alert = this.alertCtrl.create({
+    title: 'Add Quote',
+    subTitle: 'Are you sure?',
+    message: 'Are you sure you want to add the quote?',
+    buttons: [
+      {
+        text: 'Yes, add to favorites',
+        handler: () => {
+          console.log("OK");
+        }
+      },
+      {
+        text: 'No, do not add',
+        role: 'cancel',
+        handler: () => {
+          console.log("Cancel");
+        }
+      }
+    ]
+
+  });
+  alert.present();
+}
 
 }
