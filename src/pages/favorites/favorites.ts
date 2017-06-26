0ee2d8076312ef9fc516fa5e55e8fe76baf34699
joshgressman <joshgressman@gmail.com>
2017-06-26 14:50:57 -0500

    added modal to show on click on the favorites page

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController} from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 import { QuotesService } from '../../services/quotes';
-
+import { QuotePage } from '../quote/quote';
 @Component({
   selector: 'page-favorites',
   templateUrl: 'favorites.html',
@@ -10,12 +11,18 @@ export class FavoritesPage {
 
  quotes: Quote[];
 
- constructor(private quotesService: QuotesService){}
+ constructor(private quotesService: QuotesService, private modalCtrl: ModalController){}
 
   //Lifecycle hook that initializes quotes array from services
   ionViewWillEnter(){
     //getFavoriteQuotes will load a copy of the array from the service
     this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  //this method will create a modal using Modal Controller from ionic-angular
+  onViewQuote(quote: Quote){
+   const modal = this.modalCtrl.create(QuotePage, quote);
+   modal.present();
   }
 
 }
--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+ person: string;
+ text: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+constructor(private viewCtrl: ViewController, private navParams: NavParams){}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+ionViewDidLoad(){ //get data from the navParams sent to the modal from favorites
+ this.person = this.navParams.get('person');
+ this.text = this.navParams.get('text');
+}
+
+ //Method will close the modal window
+ //View controller controls the currently active view
+  onClose(){
+   this.viewCtrl.dismiss();
   }
 
 }
